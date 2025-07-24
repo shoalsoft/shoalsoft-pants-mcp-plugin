9ab3df2e6b62b498b544f70651a48b8022cdf7cf
Tom Dyas <tom@shoalsoftware.com>
2025-07-24 00:05:09 -0400

    add `test` goal as a MCP tool (#5)
    Add the `test` goal as a MCP tool. This is a really rough minimal
    implementation for demonstration purposes and, for example, does not
    expose the goal's stdout / stderr to the client nor does it provide a
    tool for each goal.

--- src/python/shoalsoft/pants_modelcontext_plugin/goals.py
+++ src/python/shoalsoft/pants_modelcontext_plugin/goals.py
@@ -17,11 +17,17 @@ import io
 import logging
 import os
 import sys
+from pathlib import Path
 
 from pants.base.exiter import ExitCode
+from pants.engine.env_vars import CompleteEnvironmentVars
+from pants.engine.goal import CurrentExecutingGoals
+from pants.engine.internals.session import SessionValues
 from pants.engine.rules import collect_rules
 from pants.goal.auxiliary_goal import AuxiliaryGoal, AuxiliaryGoalContext
+from pants.init.engine_initializer import GraphSession
 from pants.option.option_types import BoolOption
+from pants.option.options_bootstrapper import OptionsBootstrapper
 from shoalsoft.pants_modelcontext_plugin.mcp_server import setup_and_run_mcp_server
 
 logger = logging.getLogger(__name__)
@@ -39,13 +45,25 @@ class McpGoal(AuxiliaryGoal):
         help="Internal option used to invoke the MCP server. DO NOT USE DIRECTLY!",
     )
 
-    def _run_server(self) -> ExitCode:
+    def _run_server(self, graph_session: GraphSession) -> ExitCode:
+        scheduler_session = graph_session.scheduler_session.scheduler.new_session(
+            build_id="mcp",
+            dynamic_ui=False,
+            session_values=SessionValues(
+                {
+                    OptionsBootstrapper: OptionsBootstrapper.create(args=[], env={}),
+                    CompleteEnvironmentVars: CompleteEnvironmentVars([]),
+                    CurrentExecutingGoals: CurrentExecutingGoals(),
+                }
+            ),
+        )
+
         saved_stdout = sys.stdout
         saved_stdin = sys.stdin
         try:
             sys.stdout = io.TextIOWrapper(os.fdopen(sys.stdout.fileno(), "wb", buffering=0))
             sys.stdin = io.TextIOWrapper(os.fdopen(sys.stdin.fileno(), "rb", buffering=0))
-            asyncio.run(setup_and_run_mcp_server())
+            asyncio.run(setup_and_run_mcp_server(scheduler_session, build_root=Path.cwd()))
         finally:
             sys.stdout = saved_stdout
             sys.stdin = saved_stdin
@@ -56,7 +74,7 @@ class McpGoal(AuxiliaryGoal):
         context: AuxiliaryGoalContext,
     ) -> ExitCode:
         if self.run_stdio_server:
-            return self._run_server()
+            return self._run_server(context.graph_session)
 
         sys.stderr.write("TODO: Easy setup coming soon to this MCP near you!\n")
         return ExitCode(0)
